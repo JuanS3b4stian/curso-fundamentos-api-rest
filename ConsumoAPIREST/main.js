7a5a2c4fa340b53b560c39907bf5a915401abc32
Juan Sebastian <juan.mun3ra@gmail.com>
2026-04-20 16:16:50 -0500

    Classes 6 - 10

--- ConsumoAPIREST/main.js
+++ ConsumoAPIREST/main.js
@@ -1,23 +1,55 @@
-// API KEY live_0y2Ocv0c6XnnSZkts2SDpe6RtmjS7mb4Jrl7ujYMF0EJznLk9yz0Yvy6tUIA6jVB
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_0y2Ocv0c6XnnSZkts2SDpe6RtmjS7mb4Jrl7ujYMF0EJznLk9yz0Yvy6tUIA6jVB';
+const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?api_key=live_0y2Ocv0c6XnnSZkts2SDpe6RtmjS7mb4Jrl7ujYMF0EJznLk9yz0Yvy6tUIA6jVB';
 
-// Quert Parameters que se pueden usar según la documentación de la api de gatitos
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_0y2Ocv0c6XnnSZkts2SDpe6RtmjS7mb4Jrl7ujYMF0EJznLk9yz0Yvy6tUIA6jVB';
+const spanError = document.getElementById('error')
 
-// Función encapsulada que está a la espera de ser llamada
-async function reload(){
-  const res = await fetch(API_URL);
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
+  console.log('Random')
+  console.log(data)
 
-  console.log(data);
-
-  // Llamar a cada imagen según el ID asignado
-  const img1 = document.getElementById('img1');
-  const img2 = document.getElementById('img2');
-  const img3 = document.getElementById('img3');
-  img1.src = data[0].url;
-  img2.src = data[1].url;
-  img3.src = data[2].url;
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+  }
 }
 
-// Llamar a la función al cargar la página
-reload();
+async function loadFavouriteMichis() {
+  const res = await fetch(API_URL_FAVOURITES);
+  const data = await res.json();
+  console.log('Favoritos')
+  console.log(data)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
+
+async function saveFavouriteMichis() {
+  const res = await fetch(API_URL_FAVOURITES, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image_id: 'dje'
+    }),
+  });
+  const data = await res.json();
+
+  console.log('Save')
+  console.log(res)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
+
+loadRandomMichis();
+loadFavouriteMichis();
